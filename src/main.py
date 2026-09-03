9333153b984ff57a31e0215ea6f8823bf479de7c
kevin <kevin.grundhall01@gmail.com>
2026-09-03 14:52:17 +0200

    updated xml body to query for all cameras within borås

--- src/main.py
+++ src/main.py
@@ -8,17 +8,27 @@ TARGET_URL = "https://api.trafikinfo.trafikverket.se/v2/data.json"
 XML = f"""
 <REQUEST>
   <LOGIN authenticationkey="{API_KEY}" />
-  <QUERY objecttype="Camera" schemaversion="1" limit="5">
+  <QUERY objecttype="Camera" schemaversion="1" limit="20">
+    <FILTER>
+      <WITHIN name="Geometry.SWEREF99TM"
+              shape="center"
+              value="374608 6404476"
+              radius="15000" />
+    </FILTER>
   </QUERY>
 </REQUEST>
 """
 HEADER = {'Content-Type': 'application/xml'}
 def main():
     print(XML)
-    response = requests.post(TARGET_URL, data=XML, headers=HEADER)
+    response = requests.post(TARGET_URL, data=XML.encode("utf-8"), headers=HEADER)
     print(response.status_code)
     print(response.text)
 
+    #picture_url = "https://api.trafikinfo.trafikverket.se/v2/Images/data/road.infrastructure.camera/TrafficFlowCamera_39636104.jpg"
+    #response_picture = requests.get(picture_url)
+    #with open("test.jpg", "wb") as file:
+    #    file.write(response_picture.content)
 
 
 
